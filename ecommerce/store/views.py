898f4044a558fbb65a1b7a03f16f71ecd62577ac
Pablo Arteaga <dj.pablo.ac@gmail.com>
2021-03-23 19:13:30 -0500

    Formulario store terminado y iniciando cart

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,14 +1,36 @@
 from django.shortcuts import render
+from .models import *
+
 
 # Create your views here.
 def store(request):
-    context = {}
+    products = Product.objects.all()
+    context = {'products': products}
     return render(request, 'store/store.html', context)
 
+
 def cart(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, create = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_items': 0, 'get_cart_total': 0}
+
+    context = {'order': order, 'items': items}
     return render(request, 'store/cart.html', context)
 
+
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, create = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        customer = {}
+        items = []
+        order = {'get_cart_items': 0, 'get_cart_total': 0}
+
+    context = {'customer': customer, 'order': order, 'items': items}
     return render(request, 'store/checkout.html', context)
