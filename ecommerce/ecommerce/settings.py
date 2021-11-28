64a44ee069db9d7d808f66c26365a59ff9034352
Pablo Arteaga <dj.pablo.ac@gmail.com>
2021-11-27 23:48:39 -0500

    [IMP] deploy heroku 3

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,4 +128,4 @@ EMAIL_HOST_PASSWORD = 'Tu63EwkYyxcjfA5G'
 
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
